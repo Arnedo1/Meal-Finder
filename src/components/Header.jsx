c9381d39e07a95ac21576b8b4b452e5edda9c457
Jose Arnedo <arnedo1@msn.com>
2025-05-14 21:05:09 +0200

    Complete mealfinder app

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import loop from '../assets/loop.svg'
+
+export default function Header() {
+    const [ change, setChange ] = useState( '' )
+
+    const firstCap = ( str ) => {
+        if ( !str ) { return '' }
+        return str[ 0 ].toUpperCase() + str.slice( 1 )
+    }
+    const clickedEnter = ( e ) => {
+        if ( e.key === 'Enter' ) {
+            e.preventDefault()
+            console.log( { change } )
+            setChange( '' )
+        }
+    }
+    return (
+        <div className="h-40 w-full bg-red-300 flex flex-col items-center justify-center">
+            <h1 className="text-center text-3xl -mt-10  mb-1 text-white">SuperCook</h1>
+            <div className="max-w-[300px] w-9/10 h-8 rounded relative flex px-4 items-center justify-center bg-white">
+                <img src={ loop } className='h-5 absolute left-5' alt="" />
+                <input onKeyDown={ ( e ) => clickedEnter( e ) } onChange={ ( e ) => setChange( e.target.value ) } required placeholder='Voor iets in' value={ firstCap( change ) } className="ml-9 w-9/10 outline-none" type="text" />
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+Footer
+export default function Home() {
+    return (
+        <>   
+        <Header/>
+        <div className="w-full h-170 bg-white rounded-3xl -mt-10 " ></div>
+        <Footer/>
+
+
+
+        </>
+    )
+}
